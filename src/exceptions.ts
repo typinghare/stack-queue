0f6c0fc2ae74b150f99b661b54f4a277dec0064d
James Chan <jameschan312.cn@gmail.com>
2022-07-26 22:46:19 -0400

    initial

--- src/exceptions.ts
+++ src/exceptions.ts
@@ -1,11 +1,5 @@
 export class EmptyStackException extends Error {
-  constructor() {
-    super('Stack underflow.');
-  }
 }
 
 export class EmptyQueueException extends Error {
-  constructor() {
-    super('Queue underflow.');
-  }
 }
